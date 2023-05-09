63bbbe140f4db5df6d60ba27bfd75058670d7e4c
Sean Gray <seangray.dev@gmail.com>
2023-05-09 12:28:35 -0400

    add portfolio component and items

---
+++ src/app/components/PortfolioItem.js
@@ -0,0 +1,31 @@
+import portfolioData from '../../../data/portfolioItems.json';
+
+const PortfolioItem = ({
+  trackTitle,
+  image,
+  filePath,
+  songName,
+  artistName,
+}) => {
+  return (
+    <div className='bg-black relative'>
+      <img
+        className='w-full object-cover aspect-square opacity-70'
+        src={image}
+        alt={`${songName} by ${artistName}`}
+      />
+      <div className='text-white absolute bottom-0 flex flex-col gap-4 px-8 pb-2 w-full'>
+        <div className='pb-4 border-b-2'>
+          <p className='text-lg font-bold tracking-wider'>{songName}</p>
+          <p className='text-sm'>By {artistName}</p>
+        </div>
+        <audio controls controlsList='nodownload' className='mx-auto'>
+          <source src={filePath} type='audio/mpeg' />
+          Your browser does not support the audio element.
+        </audio>
+      </div>
+    </div>
+  );
+};
+
+export default PortfolioItem;
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,6 @@
 import Nav from './components/Nav';
 import Hero from './components/Hero';
+import { Portfolio } from './components/Portfolio';
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <Nav />
       <main>
         <Hero />
+        <Portfolio />
       </main>
     </>
   );
