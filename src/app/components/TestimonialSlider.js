8bc142d9b09509c7252d09745364f0b9d0fa17f0
Sean Gray <seangray.dev@gmail.com>
2023-05-10 15:48:53 -0400

    replace image with new edit, make testimonials animated and responsive

--- src/app/components/TestimonialSlider.js
+++ src/app/components/TestimonialSlider.js
@@ -32,12 +32,12 @@ const TestimonialSlider = () => {
     <div className='relative h-full'>
       <div className='relative'>
         <button
-          className='absolute flex items-center justify-center transform top-48 left-10 z-10 w-10 h-10 bg-white rounded-full hover:text-white hover:bg-black transition-colors text-4xl'
+          className='absolute flex items-center justify-center transform top-[250px] left-1 z-10 w-10 h-10 bg-white rounded-full hover:opacity-60 transition-all text-4xl'
           onClick={prevTestimonial}>
           {'<'}
         </button>
         <button
-          className='absolute flex items-center justify-center transform top-48 right-10 z-10 w-10 h-10 bg-white rounded-full hover:text-white hover:bg-black transition-colors text-4xl'
+          className='absolute flex items-center justify-center transform top-[250px] right-1 z-10 w-10 h-10 bg-white rounded-full hover:opacity-60 transition-all text-4xl'
           onClick={nextTestimonial}>
           {'>'}
         </button>
@@ -45,20 +45,24 @@ const TestimonialSlider = () => {
 
       {testimonials.map((testimonial, index) => {
         return (
-          <div key={index} className={index === current ? 'block' : 'hidden'}>
-            <img
-              className='w-full h-full max-h-[300px] lg:max-h-[400px] object-top object-cover'
-              src={testimonial.imgUrl}
-              alt={`Image of ${testimonial.client}`}
-            />
-            <div className='flex flex-col gap-2 text-white text-sm tracking-wider pt-10 pb-20 px-8'>
-              <h3 className='text-xl font-bold'>{testimonial.client}</h3>
-              <div className='flex flex-col gap-1 lg:flex-row lg:gap-4 opacity-50'>
-                <p>{testimonial.aboutClient}</p>
-                <span className='hidden lg:block'>|</span>
-                <p>{testimonial.location}</p>
+          <div
+            key={index}
+            className={index === current ? 'block fade-in' : 'hidden'}>
+            <div className='flex flex-col lg:grid lg:grid-cols-2'>
+              <img
+                className='w-full h-[500px] object-cover'
+                src={testimonial.imgUrl}
+                alt={`Image of ${testimonial.client}`}
+              />
+              <div className='flex flex-col gap-2 text-white text-sm tracking-wider p-14'>
+                <h3 className='text-xl font-bold'>{testimonial.client}</h3>
+                <div className='flex flex-col gap-1 lg:flex-row lg:gap-4 opacity-50'>
+                  <p>{testimonial.aboutClient}</p>
+                  <span className='hidden lg:block'>|</span>
+                  <p>{testimonial.location}</p>
+                </div>
+                <p className='max-w-[500px]'>{testimonial.testimonial}</p>
               </div>
-              <p className='max-w-[500px]'>{testimonial.testimonial}</p>
             </div>
           </div>
         );
