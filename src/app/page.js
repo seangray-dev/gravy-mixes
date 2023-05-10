70c775f2b31280fd216cf9dab27781dfb0ff2eaa
Sean Gray <seangray.dev@gmail.com>
2023-05-10 05:14:18 -0400

    add footer comp

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Playlists from './components/Playlists';
 import Testimonials from './components/Testimonials';
 import About from './components/About';
 import QuoteRequestForm from './components/QuoteRequestForm';
+import Footer from './components/Footer';
 
 export default function Home() {
   return (
@@ -20,6 +21,7 @@ export default function Home() {
         <About />
         <QuoteRequestForm />
       </main>
+      <Footer />
     </>
   );
 }
